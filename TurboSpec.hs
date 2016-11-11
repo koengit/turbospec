fba81c5f8790338597cd213369c76b43625a11ef
Koen Claessen <koen@chalmers.se>
2016-11-11 15:53:27 +0100

    snapshot

--- TurboSpec.hs
+++ TurboSpec.hs
@@ -1,13 +1,16 @@
 module Main where
 
+import System.IO( hFlush, stdout )
 import Data.List
 import Data.Dynamic
 import Data.Maybe
 import qualified Data.Set as S
+import qualified Data.Map as M
 import Test.QuickCheck
 import SAT hiding ( Lit )
 import SAT.Unary
 import SAT.Val
+import SAT.Equal
 
 type Var = (String,Type)
 
@@ -36,12 +39,40 @@ tBool = Type "Bool" (toDyn `fmap` (arbitrary :: Gen Bool)) (makeVars tBool ["B"]
         (\x y -> fromDyn x (undefined :: Bool) == fromDyn y (undefined :: Bool))
 
 tNat :: Type
-tNat = Type "Nat" (toDyn `fmap` (arbitrary `suchThat` (>=0) :: Gen Nat)) (makeVars tNat ["N","M"]) 
+tNat = Type "Nat" (toDyn `fmap` (arbitrary `suchThat` (>=0) :: Gen Nat)) (makeVars tNat ["N"]) 
        (\x y -> fromDyn x (undefined :: Nat) == fromDyn y (undefined :: Nat))
 
 tListNat :: Type
-tListNat = Type "[Nat]" (toDyn `fmap` (listOf (arbitrary `suchThat` (>=0)) :: Gen [Nat])) (makeVars tListNat ["Ns","Ms"])
+tListNat = Type "[Nat]" (toDyn `fmap` (listOf (arbitrary `suchThat` (>=0)) :: Gen [Nat])) (makeVars tListNat ["Ns"])
            (\x y -> fromDyn x (undefined :: [Nat]) == fromDyn y (undefined :: [Nat]))
+
+tA :: Type
+tA = Type "A" (toDyn `fmap` (arbitrary :: Gen Int)) (makeVars tA ["X"]) 
+       (\x y -> fromDyn x (undefined :: Int) == fromDyn y (undefined :: Int))
+
+tB :: Type
+tB = Type "B" (toDyn `fmap` (arbitrary :: Gen Int)) (makeVars tB ["Y"]) 
+       (\x y -> fromDyn x (undefined :: Int) == fromDyn y (undefined :: Int))
+
+tAB :: Type
+tAB = Type "(A,B)" (toDyn `fmap` (arbitrary :: Gen (Int,Int))) (makeVars tAB ["P"]) 
+       (\x y -> fromDyn x (undefined :: (Int,Int)) == fromDyn y (undefined :: (Int,Int)))
+
+tListA :: Type
+tListA = Type "[A]" (toDyn `fmap` (listOf arbitrary :: Gen [Int])) (makeVars tListA ["Xs"])
+           (\x y -> fromDyn x (undefined :: [Int]) == fromDyn y (undefined :: [Int]))
+
+tListB :: Type
+tListB = Type "[B]" (toDyn `fmap` (listOf arbitrary :: Gen [Int])) (makeVars tListB ["Ys"])
+           (\x y -> fromDyn x (undefined :: [Int]) == fromDyn y (undefined :: [Int]))
+
+tListAB :: Type
+tListAB = Type "[(A,B)]" (toDyn `fmap` (listOf arbitrary :: Gen [(Int,Int)])) (makeVars tListAB ["Ps"])
+           (\x y -> fromDyn x (undefined :: [(Int,Int)]) == fromDyn y (undefined :: [(Int,Int)]))
+
+tListAA :: Type
+tListAA = Type "[(A,A)]" (toDyn `fmap` (listOf arbitrary :: Gen [(Int,Int)])) (makeVars tListAA ["Ps"])
+           (\x y -> fromDyn x (undefined :: [(Int,Int)]) == fromDyn y (undefined :: [(Int,Int)]))
 
 data Fun
   = Fun
@@ -60,13 +91,10 @@ fun1 f [x] = toDyn (f (fromDyn x undefined))
 fun2 :: (Typeable a, Typeable b, Typeable c) => (a -> b -> c) -> [Dynamic] -> Dynamic
 fun2 f [x,y] = toDyn (f (fromDyn x undefined) (fromDyn y undefined))
 
+fun3 :: (Typeable a, Typeable b, Typeable c, Typeable d) => (a -> b -> c -> d) -> [Dynamic] -> Dynamic
+fun3 f [x,y,z] = toDyn (f (fromDyn x undefined) (fromDyn y undefined) (fromDyn z undefined))
+
 type Nat = Int
-
-f_insert :: Fun
-f_insert = Fun "insert" [tNat,tListNat] tListNat (fun2 (insert :: Nat -> [Nat] -> [Nat]))
-
-f_sort :: Fun
-f_sort = Fun "sort" [tListNat] tListNat (fun1 (sort :: [Nat] -> [Nat]))
 
 instance Ord Fun where
   f `compare` g = name f `compare` name g
@@ -113,25 +141,24 @@ eval sub ((p,xs):<=>:b) =
 eval sub (x:=:y@(_,t)) =
   eq t (fromJust (lookup x sub)) (fromJust (lookup y sub))
 
-literals :: Int -> Sig -> [Lit]
-literals k funs =
+literals :: (Type -> Int) -> Sig -> [Lit]
+literals tyNum funs =
   [ (f,xs) :/=: y
   | f  <- funs
   , result f /= tBool
-  , xs <- cross [ take k (vars t) | t <- args f ]
-  , y  <- take k (vars (result f))
+  , xs <- cross [ take (tyNum t) (vars t) | t <- args f ]
+  , y  <- take (tyNum (result f)) (vars (result f))
   ] ++
   [ (p,xs) :<=>: b
   | p <- funs
   , result p == tBool
-  , xs <- cross [ take k (vars t) | t <- args p ]
+  , xs <- cross [ take (tyNum t) (vars t) | t <- args p ]
   , b  <- [False,True]
   ] ++
   [ x :=: y
   | t <- types
-  , x <- take k (vars t)
-  , y <- take k (vars t)
-  , x < y 
+  , (x,i) <- take (tyNum t) (vars t) `zip` [1..]
+  , y     <- drop i $ take (tyNum t) (vars t)
   ]
  where
   types = map head . group . sort $
@@ -152,7 +179,7 @@ vec (x      :=: y)   = [x,y]
 pre :: Type -> Lit -> Maybe Var
 pre typ lit
   | null xs   = Nothing
-  | otherwise = gap xs (reverse (takeWhile (/=x) (vars typ)))
+  | otherwise = gap (takeWhile (/=x) xs) (reverse (takeWhile (/=x) (vars typ)))
  where
   xs = nub [ x | x@(_,t) <- vec lit, t == typ ]
   x  = maxi (vars typ) xs
@@ -163,7 +190,20 @@ pre typ lit
   gap have []          = Nothing
   gap have (w:want)
     | w `notElem` have = Just w
-    | otherwise        = gap have want
+    | otherwise        = gap (takeWhile (/=w) have) want
+
+(<<) :: Lit -> Lit -> Bool
+lit1 << lit2 = prnt lit1 < prnt lit2
+ where
+  prnt lit@(_:=:_)       = Nothing
+  prnt lit@((f,_):/=:_)  = Just (f, norm M.empty 0 (vec lit))
+  prnt lit@((p,_):<=>:_) = Just (p, norm M.empty 0 (vec lit))
+
+  norm tab i []     = []
+  norm tab i (x:xs) =
+    case M.lookup x tab of
+      Nothing -> i : norm (M.insert x i tab) (i+1) xs
+      Just j  -> j : norm tab i xs
 
 type Model = ([(Fun,[Int],Int)],[(Fun,[Int],Bool)])
 
@@ -192,38 +232,93 @@ speculate lits (fmod,pmod) =
          , y `notElem` xs
          ]
 
-       -- removing symmetries
+       -- no twice definitions
        sequence_
-         [ do addClause s (neg l1 : [ l2 | (lit2,l2) <- tab, x `elem` vec lit2 ])
-              --print (lit1,[ lit2 | (lit2,l2) <- tab, lit2 < lit1, x `elem` vec lit2 ])
+         [ addClause s [neg l1, neg l2]
+         | (fxs :/=:x,l1) <- tab
+         , (fxs':/=:y,l2) <- tab
+         , fxs == fxs'
+         , x /= y
+         ]
+
+       -- no free variables in equations
+       sequence_
+         [ do addClause s (neg l1 : [ l2 | (lit2,l2) <- tab, notEq lit2, x `elem` vec lit2 ])
+              addClause s (neg l1 : [ l2 | (lit2,l2) <- tab, notEq lit2, y `elem` vec lit2 ])
+         | (x:=:y,l1) <- tab
+         , let notEq (_:=:_) = False
+               notEq _       = True
+         ]
+
+       -- removing symmetries
+       putStrLn ("-- removing symmetries...")
+       sequence_
+         [ do putStrLn (show lit1 ++ " --> " ++ fst x)
+              addClause s (neg l1 : [ l2 | (lit2,l2) <- tab, l1 /= l2, not (lit1 << lit2), x `elem` vec lit2 ])
          | (lit1,l1) <- tab
          , typ <- nub [ t | (_,t) <- vec lit1 ]
          , Just x <- [pre typ lit1]
          ]
 
        -- adding test cases
-       let addTests 0 =
-             do return ()
+       putStrLn ("-- adding test cases...")
+       let tests' seen i j sub | i >= 300 =
+             do putStrLn ("(" ++ show (S.size seen) ++ " clauses)")
+                return seen
            
-           addTests i =
-             do as <- sequence [ generate (gen t) | (_,t) <- vs ]
-                refine S.empty i (vs `zip` as)
+           tests' seen i j sub | j >= 5*length lits =
+             do tests seen i
            
-           refine seen i sub
-             | ls `S.member` seen = do addTests (i-1)
-             | otherwise          = do --print [ (lit,eval sub lit) | lit <- lits ]
-                                       --print [ lit | (lit,l) <- tab, l `elem` ls ]
-                                       addClause s ls
-                                       ((f,xs):/=:y) <- generate $ elements [ lit | lit@(_:/=:_) <- lits ]
-                                       let b = app f [ a | x <- xs, Just a <- [lookup x sub] ]
-                                       refine (S.insert ls seen) i
-                                              [ if x == y then (y,b) else (x,a) | (x,a) <- sub ]
+           tests' seen i j sub =
+             do new <- solve s (map neg cl)
+                let lits1 = [ lit | (lit@(_:/=:_),True) <- lits `zip` bs ]
+                (y,b) <- if not (null lits1) then
+                           do ((f,xs):/=:y) <- generate $ elements lits1
+                              let b = app f [ a | x <- xs, Just a <- [lookup x sub] ]
+                              return (y,b)
+                         else
+                           do y <- generate $ elements vs
+                              b <- generate (resize (i `mod` 100) (gen (snd y)))
+                              return (y,b)
+                let sub' = [ if x == y then (y,b) else (x,a) | (x,a) <- sub ]     
+                if new then
+                  do putStr "."
+                     hFlush stdout
+                     addClause s cl
+                     tests' (S.insert cl seen) 0 0 sub'
+                 else
+                  do tests' seen i (j+1) sub'
             where
-             ls = [ l | (lit,l) <- tab, eval sub lit ]
+             bs = [ eval sub lit | lit <- lits ]
+             cl = [ l | (l,True) <- ls `zip` bs ]
            
-       addTests 10000
+           tests seen i =
+             do putStr "#"
+                hFlush stdout
+                as <- sequence [ generate (resize (i `mod` 100) (gen t)) | (_,t) <- vs ]
+                tests' seen (i+1) 0 (vs `zip` as)
+           
+       tsts <- tests S.empty 0
+       
+       -- every speculated clause must be (locally) minimal
+       sequence_
+         [ do xs <- sequence [ newLit s | c <- cs ]
+              addClause s (neg l : xs)
+              sequence_
+                [ addClause s [neg x, neg l']
+                | (x,c) <- xs `zip` cs
+                , l' <- c
+                , l' /= l
+                ]
+         | (l,cs) <- M.toList $ M.fromListWith (++)
+                     [ (l,[ls])
+                     | ls <- S.toList tsts
+                     , l <- ls
+                     ]
+         ]
        
        -- adding model constraints
+       putStrLn ("-- adding model constraints...")
        let tas = [ (t, nub $ [ a
                              | (f,as,b) <- fmod
                              , (a,t') <- (b:as) `zip` (result f:args f)
@@ -235,11 +330,14 @@ speculate lits (fmod,pmod) =
                              , t' == t
                              ])
                  | t <- ts
-                 ]
+                 , t /= tBool
+                 ] ++
+                 [ (tBool, [0,1]) ]
        vals <- sequence
-         [ newVal s as
+         [ do putStrLn (fst v ++ " = " ++ show as)
+              newVal s as
          | v@(_,t) <- vs
-         , let as:_ = [ as | (t',as) <- tas, t' == t ]
+         , [as] <- [[ as | (t',as) <- tas, t' == t ]]
          ]
        let vtab = vs `zip` vals
 
@@ -269,11 +367,18 @@ speculate lits (fmod,pmod) =
          , let cases = [ as | (p',as,b') <- pmod, p' == p, b' /= b ]
          ]
 
+       sequence_
+         [ do notEqualOr s [neg l] a b
+         | (x:=:y,l) <- tab
+         , [a] <- [[ v | (x',v) <- vtab, x' == x ]]
+         , [b] <- [[ v | (y',v) <- vtab, y' == y ]]
+         ]
+
        -- counting literals
        n <- count s ls
 
        -- looping
-       let loop k | k > 5 =
+       let loop k | k > 10 =
              do putStrLn "GIVE UP"
                 return Nothing
        
@@ -282,7 +387,10 @@ speculate lits (fmod,pmod) =
                 b <- solve s [n .<= k]
                 if b then
                   do bs <- sequence [ SAT.modelValue s l | l <- ls ]
-                     return (Just [ lit | (lit,True) <- lits `zip` bs ])
+                     print [ lit | (lit,True) <- lits `zip` bs ]
+                     addClause s (map neg [ l | (l,True) <- ls `zip` bs ])
+                     --return (Just [ lit | (lit,True) <- lits `zip` bs ])
+                     loop k
                  else
                   do putStrLn ("-- solving (k=" ++ show k ++ ")")
                      loop (k+1)
@@ -290,4 +398,117 @@ speculate lits (fmod,pmod) =
  where
   vs = nub [ v | lit <- lits, v <- vec lit ]
   ts = nub [ t | (_,t) <- vs ]
+
+main1 =
+  do c <- speculate (literals tyNum sig) (fmod,pmod)
+     print c
+ where
+  sig = [f_insert,f_sort,f_sorted]
+
+  a = 1
+  as = 2
+  sort_as = 3
+  sort_aas = 4
+  aas = 5
+  
+  fmod = [ (f_sort,[as],sort_as)
+         , (f_sort,[aas],sort_aas)
+         , (f_insert,[a,sort_as],sort_aas)
+         ]
+  
+  pmod = [ (f_sorted,[sort_as],True)
+         , (f_sorted,[sort_aas],False)
+         ]
+
+tyNum t | t == tBool    = 2
+tyNum t | t == tNat     = 2
+tyNum t | t == tA       = 3
+tyNum t | t == tB       = 2
+tyNum t | t == tAB      = 2
+tyNum t | t == tListNat = 2
+tyNum t | t == tListA   = 2
+tyNum t | t == tListB   = 2
+tyNum t | t == tListAB  = 2
+tyNum t | t == tListAA  = 2
+
+main2 =
+  do c <- speculate (literals tyNum sig) (fmod,pmod)
+     print c
+ where
+  sig = [f_rev,f_zip,f_lenAA,f_lenA]
+
+  as:cs:ras
+    :zip_as_cs:zip_as_ras
+    :len_as:len':_
+    = [1..]
+
+  fmod = [ (f_zip, [as,cs],  zip_as_cs)
+         , (f_zip, [as,ras], zip_as_ras)
+         
+         , (f_rev, [as], ras)
+         
+         , (f_lenA, [as],  len_as)
+         , (f_lenA, [ras], len_as)
+         , (f_lenA, [cs],  len_as)
+         
+         , (f_lenAA, [zip_as_ras], len_as)
+         , (f_lenAA, [zip_as_cs],  len')
+         ]
+  
+  pmod = []
+
+main =
+  do c <- speculate (literals tyNum sig) (fmod,pmod)
+     print c
+ where
+  sig = [f_memb,f_swap]
+
+  a:b:c:d
+   :das:swap_das:_
+    = [1..]
+
+  fmod = [ (f_swap, [a,b,das], swap_das)
+         ]
+  
+  pmod = [ (f_memb, [a, das], True)
+         , (f_memb, [b, das], True)
+         , (f_memb, [c, das], True)
+         , (f_memb, [c, swap_das], False)
+         ]
+
+f_insert :: Fun
+f_insert = Fun "insert" [tA,tListA] tListA (fun2 (insert :: Nat -> [Nat] -> [Nat]))
+
+f_sort :: Fun
+f_sort = Fun "sort" [tListA] tListA (fun1 (sort :: [Nat] -> [Nat]))
+
+f_sorted :: Fun
+f_sorted = Fun "sorted" [tListA] tBool (fun1 (\xs -> sort xs == (xs :: [Nat])))
+
+f_rev :: Fun
+f_rev = Fun "rev" [tListB] tListB (fun1 (reverse :: [Int] -> [Int]))
+
+f_zip :: Fun
+f_zip = Fun "zip" [tListA,tListA] tListAA (fun2 (zip :: [Int] -> [Int] -> [(Int,Int)]))
+
+f_lenA :: Fun
+f_lenA = Fun "lenA" [tListA] tNat (fun1 (length :: [Int] -> Int))
+
+f_lenB :: Fun
+f_lenB = Fun "lenB" [tListB] tNat (fun1 (length :: [Int] -> Int))
+
+f_lenAB :: Fun
+f_lenAB = Fun "lenAB" [tListAB] tNat (fun1 (length :: [(Int,Int)] -> Int))
+
+f_lenAA :: Fun
+f_lenAA = Fun "lenAA" [tListAA] tNat (fun1 (length :: [(Int,Int)] -> Int))
+
+f_memb :: Fun
+f_memb = Fun "memb" [tA,tListA] tBool (fun2 (elem :: Int -> [Int] -> Bool))
+
+f_swap :: Fun
+f_swap = Fun "swap" [tA,tA,tListA] tListA (fun3 (swap :: Int -> Int -> [Int] -> [Int]))
+ where
+  swap x y xs = [ if z == x then y else if z == y then x else z | z <- xs ]
+
 
